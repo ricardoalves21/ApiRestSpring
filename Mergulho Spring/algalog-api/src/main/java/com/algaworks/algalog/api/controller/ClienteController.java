f2df7058a50d53e4da49c13dc30a5a27e9a9fd55
Ricardo Alves <dev.rical21@gmail.com>
2022-11-25 09:21:47 -0300

    Apredendo sobre Validation

--- Mergulho Spring/algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ Mergulho Spring/algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -3,6 +3,8 @@ package com.algaworks.algalog.api.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -42,12 +44,12 @@ public class ClienteController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionar(@RequestBody Cliente cliente) {
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
 	
 	@PutMapping("/{clienteId}")
-	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente) {
+	public ResponseEntity<Cliente> atualizar(@Valid @PathVariable Long clienteId, @RequestBody Cliente cliente) {
 		
 		if(!clienteRepository.existsById(clienteId)) {
 			return ResponseEntity.notFound().build();
--- Mergulho Spring/algalog-api/src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ Mergulho Spring/algalog-api/src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -5,6 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -21,12 +25,19 @@ public class Cliente {
 	private Long id;
 	
 	@Column
+	@NotBlank
+	@Size(max = 60)
 	private String nome;
 		
 	@Column
+	@NotBlank
+	@Email
+	@Size(max = 255)
 	private String email;
 	
 	@Column(name = "fone")
+	@NotBlank
+	@Size
 	private String telefone;
 
 }
