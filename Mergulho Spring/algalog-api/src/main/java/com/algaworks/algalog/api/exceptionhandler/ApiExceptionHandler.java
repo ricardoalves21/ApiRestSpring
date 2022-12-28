a1e57d4ae95f2b54dc90669908abed75c8f09074
Ricardo Alves <dev.rical21@gmail.com>
2022-12-28 09:05:51 -0300

    Melhorando o retorno das Exceptions

--- Mergulho Spring/algalog-api/src/main/java/com/algaworks/algalog/api/exceptionhandler/ApiExceptionHandler.java
+++ Mergulho Spring/algalog-api/src/main/java/com/algaworks/algalog/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.algaworks.algalog.api.exceptionhandler;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -17,10 +21,21 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 		
+		List<Problema.Campo> campos = new ArrayList<>();
+		
+		for(ObjectError error : ex.getBindingResult().getAllErrors()) {
+			
+			String nome = ((FieldError) error).getField(); 
+			String mensagem = error.getDefaultMessage();
+			
+			campos.add(new Problema.Campo(nome, mensagem));
+		}
+		
 		Problema problema = new Problema();
 		problema.setStatus(status.value());
 		problema.setDataHora(LocalDateTime.now());
 		problema.setTitulo("Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.");
+		problema.setCampos(campos);
 		
 		return handleExceptionInternal(ex, problema, headers, status, request);
 	}
--- Mergulho Spring/algalog-api/src/main/java/com/algaworks/algalog/api/exceptionhandler/Problema.java
+++ Mergulho Spring/algalog-api/src/main/java/com/algaworks/algalog/api/exceptionhandler/Problema.java
@@ -1,7 +1,9 @@
 package com.algaworks.algalog.api.exceptionhandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,5 +14,16 @@ public class Problema {
 	private Integer status;
 	private LocalDateTime dataHora;
 	private String titulo;	
+	private List<Campo> campos;
+	
+	
+	@AllArgsConstructor
+	@Getter
+	public static class Campo {
+		
+		private String nome;
+		private String mensagem;
+		
+	}
 
 }
