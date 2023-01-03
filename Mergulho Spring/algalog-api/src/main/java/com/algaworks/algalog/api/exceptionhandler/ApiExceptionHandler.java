66724ec5d3a584dcbd2dd4b368556c6d95e2522a
Ricardo Alves <dev.rical21@gmail.com>
2023-01-03 08:53:42 -0300

    Implementando o arquivo 'messages.properties'

--- Mergulho Spring/algalog-api/src/main/java/com/algaworks/algalog/api/exceptionhandler/ApiExceptionHandler.java
+++ Mergulho Spring/algalog-api/src/main/java/com/algaworks/algalog/api/exceptionhandler/ApiExceptionHandler.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +16,13 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	private MessageSource messageSource;
 	
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
@@ -26,7 +33,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		for(ObjectError error : ex.getBindingResult().getAllErrors()) {
 			
 			String nome = ((FieldError) error).getField(); 
-			String mensagem = error.getDefaultMessage();
+			String mensagem = messageSource.getMessage(error, LocaleContextHolder.getLocale());
 			
 			campos.add(new Problema.Campo(nome, mensagem));
 		}
