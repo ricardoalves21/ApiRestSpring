8f4b1ca3da31c2b4f0a6629147be0569f53eab0b
Ricardo Alves <dev.rical21@gmail.com>
2023-01-12 09:21:39 -0300

    Criando as tabelas entrega e destinatario

---
+++ Mergulho Spring/algalog-api/src/main/java/com/algaworks/algalog/api/controller/EntregaController.java
@@ -0,0 +1,15 @@
+package com.algaworks.algalog.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/entregas")
+public class EntregaController {
+	
+	// parei no vídeo 3.1 no minuto 25:00
+
+}
