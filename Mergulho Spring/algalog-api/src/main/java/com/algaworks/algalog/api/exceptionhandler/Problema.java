38e7e8f6f01c1b0f997d2a38b2a332ca7487bee2
Ricardo Alves <dev.rical21@gmail.com>
2023-02-15 09:19:01 -0300

    Criando a camada DTO

--- Mergulho Spring/algalog-api/src/main/java/com/algaworks/algalog/api/exceptionhandler/Problema.java
+++ Mergulho Spring/algalog-api/src/main/java/com/algaworks/algalog/api/exceptionhandler/Problema.java
@@ -1,6 +1,6 @@
 package com.algaworks.algalog.api.exceptionhandler;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -16,7 +16,7 @@ import lombok.Setter;
 public class Problema {
 	
 	private Integer status;
-	private LocalDateTime dataHora;
+	private OffsetDateTime dataHora;
 	private String titulo;	
 	private List<Campo> campos;
 	
--- Mergulho Spring/algalog-api/src/main/java/com/algaworks/algalog/domain/service/SolicitacaoEntregaService.java
+++ Mergulho Spring/algalog-api/src/main/java/com/algaworks/algalog/domain/service/SolicitacaoEntregaService.java
@@ -1,15 +1,13 @@
 package com.algaworks.algalog.domain.service;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.algaworks.algalog.domain.exception.NegocioException;
 import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.model.Entrega;
 import com.algaworks.algalog.domain.model.StatusEntrega;
-import com.algaworks.algalog.domain.repository.ClienteRepository;
 import com.algaworks.algalog.domain.repository.EntregaRepository;
 
 import lombok.AllArgsConstructor;
@@ -27,7 +25,7 @@ public class SolicitacaoEntregaService {
 		Cliente cliente = catalogoClienteService.buscar(entrega.getCliente().getId());
 		entrega.setCliente(cliente);
 		entrega.setStatusEntrega(StatusEntrega.PENDENTE);
-		entrega.setDataPedido(LocalDateTime.now());		
+		entrega.setDataPedido(OffsetDateTime.now());		
 		
 		return entregaRepository.save(entrega);
 	}
