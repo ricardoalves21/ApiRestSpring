12d4114e1a3df997c9209d4864376a7be81441fd
Ricardo Alves <dev.rical21@gmail.com>
2023-02-22 09:15:15 -0300

    Configurando a classe @Bean para ModelMapper

---
+++ Mergulho Spring/algalog-api/src/main/java/com/algaworks/algalog/common/ModelMapperConfig.java
@@ -0,0 +1,18 @@
+package com.algaworks.algalog.common;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+}
+
+
+// parei no vídeo 3.5 no minuto 7:15
