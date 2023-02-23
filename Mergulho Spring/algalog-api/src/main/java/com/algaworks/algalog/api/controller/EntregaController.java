8ba83958d33266684ee52897fd19152c296568f6
Ricardo Alves <dev.rical21@gmail.com>
2023-02-23 08:49:26 -0300

    Separando o domain.model do representation.model

--- Mergulho Spring/algalog-api/src/main/java/com/algaworks/algalog/api/controller/EntregaController.java
+++ Mergulho Spring/algalog-api/src/main/java/com/algaworks/algalog/api/controller/EntregaController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algalog.api.assembler.EntregaAssembler;
 import com.algaworks.algalog.api.model.EntregaModel;
 import com.algaworks.algalog.domain.model.Entrega;
 import com.algaworks.algalog.domain.repository.EntregaRepository;
@@ -29,28 +29,24 @@ public class EntregaController {
 	
 	private EntregaRepository entregaRepository;
 	private SolicitacaoEntregaService solicitacaoEntregaService;
-	private ModelMapper modelMapper;
+	private EntregaAssembler entregaAssembler;
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Entrega solicitar(@Valid @RequestBody Entrega entrega) {
-		return solicitacaoEntregaService.solicitar(entrega);
+	public EntregaModel solicitar(@Valid @RequestBody Entrega entrega) {
+		Entrega entregaSolicitada = solicitacaoEntregaService.solicitar(entrega);
+		return entregaAssembler.toModel(entregaSolicitada);
 	}
 	
 	@GetMapping
-	public List<Entrega> listar() {				
-		return entregaRepository.findAll();		
+	public List<EntregaModel> listar() {				
+		return entregaAssembler.toCollectionModel(entregaRepository.findAll());		
 	}
 	
 	@GetMapping("/{entregaId}")
 	public ResponseEntity<EntregaModel> buscar(@PathVariable Long entregaId) {
-		return entregaRepository.findById(entregaId).map(entrega -> {
-			
-					EntregaModel entregaModel = modelMapper.map(entrega, EntregaModel.class);
-					
-					return ResponseEntity.ok(entregaModel);
-					
-				}).orElse(ResponseEntity.notFound().build());
-	}
-
+		return entregaRepository.findById(entregaId)
+				.map(entrega -> ResponseEntity.ok(entregaAssembler.toModel(entrega)))
+				.orElse(ResponseEntity.notFound().build());
+				}
 }
