d296bc669703cb351993f09ec9d3ff0ea22f6e4e
Ricardo Alves <dev.rical21@gmail.com>
2023-03-07 08:40:33 -0300

    Criando classes para tratar exceções de ocorrencias

--- Mergulho Spring/algalog-api/src/main/java/com/algaworks/algalog/domain/service/BuscaEntregaService.java
+++ Mergulho Spring/algalog-api/src/main/java/com/algaworks/algalog/domain/service/BuscaEntregaService.java
@@ -2,7 +2,7 @@ package com.algaworks.algalog.domain.service;
 
 import org.springframework.stereotype.Service;
 
-import com.algaworks.algalog.domain.exception.NegocioException;
+import com.algaworks.algalog.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algalog.domain.model.Entrega;
 import com.algaworks.algalog.domain.repository.EntregaRepository;
 
@@ -17,8 +17,6 @@ public class BuscaEntregaService {
 	public Entrega buscar(Long entregaId) {		
 		
 		return entregaRepository.findById(entregaId)
-				.orElseThrow(() -> new NegocioException("Entrega não encontrada"));		
+				.orElseThrow(() -> new EntidadeNaoEncontradaException("Entrega não encontrada"));		
 	}
 }
-
-// Parei no vídeo 3.6 no minuto 25:00
